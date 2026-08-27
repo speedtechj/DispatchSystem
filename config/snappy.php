2fafbfd523ae202a1917adaa0b1e726c194c32bb
speedtechj <arbiasjr@gmail.com>
2026-08-26 18:02:23 -0600

    update

--- config/snappy.php
+++ config/snappy.php
@@ -10,15 +10,15 @@ return [
     | This option contains settings for PDF generation.
     |
     | Enabled:
-    |    
+    |
     |    Whether to load PDF / Image generation.
     |
     | Binary:
-    |    
+    |
     |    The file path of the wkhtmltopdf / wkhtmltoimage executable.
     |
     | Timeout:
-    |    
+    |
     |    The amount of time to wait (in seconds) before PDF / Image generation is stopped.
     |    Setting this to false disables the timeout (unlimited processing time).
     |
@@ -32,17 +32,17 @@ return [
     |    The environment variables to set while running the wkhtmltopdf process.
     |
     */
-    
+
     'pdf' => [
         'enabled' => true,
-       'binary' => env('WKHTML_PDF_BINARY', '/usr/bin/wkhtmltopdf'),
-  // ///  ////    'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+    //   'binary' => env('WKHTML_PDF_BINARY', '/usr/bin/wkhtmltopdf'),
+    'binary'  => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
       // 'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
         'timeout' => false,
         'options' => ['enable-local-file-access' => true,],
         'env'     => [],
     ],
-    
+
     'image' => [
         'enabled' => true,
         'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
